df478a4cd2a3542a3a6de1dc28efb908fa68dc15
MechanicalMouse <LauraG0@hotmail.com>
2018-11-28 18:07:16 -0700

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+var topics = ["hamster", "space", "food", "raichu", "tea"]
+
+
+//var queryURL = `https://api.giphy.com/v1/gifs/search?q=${topics}&api_key=UA4jHqQhXxYph6oiBOoI7fl8fNLsuZIt`;
+
+//$.ajax({
+//  url: queryURL,
+// method: "GET"
+// })
+
+
+function renderButtons() {    
+       
+    $("#buttons").empty();
+
+       
+       for (var i = 0; i < topics.length; i++) {
+
+         
+         var a = $("<button>");
+         
+         a.addClass("favButton");
+         
+         a.attr("data-name", topics[i]);
+         
+         a.text(topics[i]);
+         
+         $("#buttons").append(a);
+       }
+}
+
+     
+$("#add-button").on("click", function(event) {
+       
+    event.preventDefault();
+
+      
+    var topic = $("#button-input").val().trim();
+       
+    topics.push(topic);
+
+       
+    renderButtons();
+});
+
+
+renderButtons();
+
+
